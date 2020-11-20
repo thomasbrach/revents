7308a9825d419be0d56ff5b8f9799c8257ae23ef
ironsocks <brach.thomasja@gmail.com>
2020-11-20 17:15:41 +0100

    initial states for form

---
+++ src/features/nav/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Container, Menu, Button } from "semantic-ui-react";
+
+const NavBar = ({ setFormOpen }) => {
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "15px" }}
+          />
+          Re-vents
+        </Menu.Item>
+        <Menu.Item name="Events" />
+        <Menu.Item>
+          <Button
+            onClick={() => setFormOpen(true)}
+            positive
+            inverted
+            content="Create Event"
+          />
+        </Menu.Item>
+        <Menu.Item position="right">
+          <Button basic inverted content="Login" />
+          <Button
+            basic
+            inverted
+            content="Register"
+            style={{ marginLeft: "0.5em" }}
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
+
+export default NavBar;
