418fa072495e8fa4292eb0dcf3e6a793d45f20e2
ironsocks <brach.thomasja@gmail.com>
2020-12-07 17:07:40 +0100

    added map key to env

--- src/features/events/eventDetailed/EventDetailedMap.jsx
+++ src/features/events/eventDetailed/EventDetailedMap.jsx
@@ -13,7 +13,7 @@ const EventDetailedMap = ({ latLng }) => {
     <Segment attached="bottom" style={{ padding: 0 }}>
       <div style={{ height: 300, width: "100%" }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
           center={latLng}
           zoom={zoom}
         >
